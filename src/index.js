987088289f0ba3f58dde88603b427b86d7ed1fbd
Kohize <kohi881@gmail.com>
2024-10-15 19:39:44 +0300

    map layout and function

--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ async function getLocation() {
     const data = response.json();
     return data
 }
+var map = L.map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 
 function printLocationData() {
     getLocation()
@@ -20,9 +30,20 @@ function printLocationData() {
             location.textContent = `${value.location.city} ${value.location.country}`;
             timezone.textContent = parseFloat(value.location.timezone);
             isp.textContent = value.isp;
+            map.panTo(new L.LatLng(value.location.lat, value.location.lng), 8);
+
         })
 
+
     clearForm()
+}
+
+function ValidateIPaddress(ipaddress) {
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
+        return (true)
+    }
+    alert("You have entered an invalid IP address!")
+    return (false)
 }
 
 formButton.addEventListener('click', printLocationData);
@@ -30,5 +51,5 @@ formButton.addEventListener('click', printLocationData);
 function clearForm() {
     searchInput.value = '';
     searchInput.textContent = ''
+    map = undefined;
 }
-
